2626645695f4f8f4738356765d2f5f92c2c70c1d
Rahul Agarwal <rahul.agarwal@cloudkitchens.com>
2024-01-04 14:40:19 -0800

    Add Cypress configuration and support files

---
+++ cypress/e2e/bento-buzz/app.cy.ts
@@ -0,0 +1,32 @@
+import { decode } from "he";
+
+describe('Bento Buzz', () => {
+    beforeEach(() => {
+        cy.fixture('articles.json').as('articles').then((articles) => {
+            cy.intercept('GET', 'https://bento-buzz.onrender.com', articles).as('getArticles');
+        });
+        cy.visit('http://localhost:3000/');
+    });
+
+    it('should navigate user to the github page', () => {
+        cy.get('a[href="https://github.com/eccentricdz/bento-buzz"]').contains('Github').should('have.attr', 'target', '_blank');
+    });
+
+    it('should intercept request for articles', () => {
+        cy.wait('@getArticles').then((interception) => {
+            cy.get('@articles').then((articles) => {
+                expect(interception.response.statusCode).to.equal(200);
+                expect(interception.response.body).to.deep.equal(articles);
+            });
+        });
+    })
+
+    it('should display articles', () => {
+        cy.get<Array<any>>('@articles').then((articles) => {
+            cy.get('article').should('have.length', articles.length);
+            articles.forEach(({ link, title }) => {
+                cy.get('article').contains(decode(title)).parent().parent().should('have.attr', 'href', link).should('have.attr', 'target', '_blank');
+            });
+        });
+    });
+});
