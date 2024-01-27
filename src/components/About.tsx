b1306b9f5824487f8cd9ce5083a63f34d748e15d
Rahul Agarwal <rahul.agarwal@cloudkitchens.com>
2024-01-26 18:06:18 -0800

    Refactor About component description

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,39 +14,13 @@ export const About = () => (
       </section>
       <h1>About</h1>
       <p className="description">
-        Bento Buzz employs AI to analyze the most impactful 1000 news stories
-        daily, rating them on a significance scale from 0 to 10. Only articles
-        with a rating exceeding <mark>7</mark> are curated for presentation. The
-        assessment of significance is based on seven crucial factors:
-        <br />
-        <br />
-        1. <mark>Scope</mark>: The number of individuals affected by the event.
-        <br />
-        <br />
-        2. <mark>Intensity</mark>: The magnitude of the event's impact.
-        <br />
-        <br />
-        3. <mark>Originality</mark>: The unexpected or distinctive nature of the
-        event.
-        <br />
-        <br />
-        4. <mark>Immediacy</mark>: The temporal proximity of the event.
-        <br />
-        <br />
-        5. <mark>Practicability</mark>: The likelihood that readers can take
-        actionable steps for personal benefit in response to the news.
-        <br />
-        <br />
-        6. <mark>Positivity</mark>: An evaluation of the event's positive
-        aspects, counteracting media negativity bias.
-        <br />
-        <br />
-        7. <mark>Credibility</mark>: An assessment of the source's reliability.
-        <br />
-        <br />
-        Bento Buzz utilizes these multidimensional criteria to sift through the
-        news landscape, offering a refined selection that ensures both substance
-        and relevance for the informed reader.
+        Bento Buzz uses AI to go through 1000 news stories every day, giving
+        them a score from 0 to 10 based on how important they are. Only stories
+        with a score higher than 7 make the cut. It considers factors like the
+        number of people affected, the impact size, uniqueness, recency, reader
+        actionability, positive aspects, and source reliability. This helps
+        Bento Buzz pick out the most interesting and important stories for its
+        readers.
       </p>
     </article>
   </section>
